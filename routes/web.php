408d1c8656af157e27799d7bcbeebec567b1d4dd
Jens Vercruysse <jensvercruysse@outlook.com>
2019-09-26 17:31:48 +0200

    added sensor routes with ressource

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resources(['rooms' => 'RoomController']);
+Route::resources(['sensors' => 'SensorController']);
