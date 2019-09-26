8a8fe11ab752f9b1f71a97ec11aedcabc68018a2
Jens Vercruysse <jensvercruysse@outlook.com>
2019-09-26 17:43:22 +0200

    added sensor migration

---
+++ database/migrations/2019_09_26_153231_create_sensors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSensorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('dev_eui')->unique();
+            $table->double('temperature');
+            $table->double('humidity');
+            $table->integer('light');
+            $table->integer('motion');
+            $table->double('battery')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensors');
+    }
+}
